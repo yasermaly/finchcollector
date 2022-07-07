3485f73b4480919edc9668e261c1de82c1a4d5ca
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-07-06 19:22:57 -0700

    wip-creates new models and views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+# Create your models here.
+
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+  # Create a cat_id FK
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+      # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
 
 # Create your models here.
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
-
+# Add the following import
 from django.http import HttpResponse
 
 # Define the home view
@@ -15,21 +17,28 @@ def about(request):
   return render(request, 'about.html')
 
 
-# Add the Cat class & list and view function below the imports
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description):
-    self.name = name
-    self.breed = breed
-    self.description = description
-
-
-finches = [
-    Finch('Lola', 'blue finch', 'The blue finch is a small tanager originating in South America, specifically in Brazil and Bolivia. The males have a bright cobalt blue plumage, although after the molt the feathers have a rufous-brown hue with broad edges. Females, on the other hand, have a rufous brown upper plumage, with buffy white underparts marked with dusky streaks.'),
-    Finch('zippy', 'safron finch', 'Saffron finch is a small tanager originating in South America. The males with a vibrant yellow body and an orange crown can be distinguished from the females that usually have a somewhat duller coloration. However, females in some subspecies have an olive-brown body consisting of heavy dark markings.'),
-    Finch('Raven', 'gouldian finch', 'The Gouldian finch, also known as rainbow finch, Lady Gouldian finch, Goulds finch, Scarlet-headed finch is an attractive and colorful bird. These birds, with a blackish beak have red, yellow or black colored heads because of their color mutations. Female finches have a less bright hue than their male counterparts and are characterized with a light mauve chest. Being classified as endangered in the wild, by the IUCN in 1992, most of these birds are bred in captivity mainly in Australia.'),
-    Finch('tweets', 'strawberry finch', 'The strawberry finch is an attractive sparrow-sized bird, originating from Asia. Characterized with colorful plumage, a vivid red bill, and a rounded black tail, it is a popular choice in aviculture. With rapid wing beats, they prefer to fly in small flocks, descending into clumping grasses, where they are not spotted easily. Unlike the females, the males sport a scarlet breeding plumage (during April-November).'),
-]
-
-
 def finches_index(request):
-    return render(request, 'finches/index.html', {'finches': finches})
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
+
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {'finch': finch})
+
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finches/'
+
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a finch by excluding the name field!
+  fields = ['breed', 'description']
+
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
